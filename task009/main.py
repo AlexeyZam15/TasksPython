aede7131cf0afac99348ade0d267ee954fe8b7a4
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-29 22:16:07 +1000

    completed task009

--- task009/main.py
+++ task009/main.py
@@ -1,19 +1,20 @@
-# Напишите программу, которая вычислит:
-# Сколько раз встретятся часовая и минутная стрелка механических часов с 0:05 до 23:55
-# Если часовая стрелка двигается только после того как минутная пройдёт отрезок в 60 минут.
+# Напишите программу, которая:
+# Вычислит сколько раз встретятся часовая и минутная стрелка механических часов с 0:05 до 23:55.
 
 def count_meet_hour_and_minute_hands(start_hour, start_minute, end_hour, end_minute):
     count = 0
-    end_minute_1 = 60
+    end_minute_1 = 59
     i = 1 if start_hour > 12 else 0
     while i < 2:
         hours = start_hour
         while hours < 12:
+            hour_meet = False
             if hours + 12 * i == end_hour:
                 end_minute_1 = end_minute
             minutes = start_minute
             while minutes <= end_minute_1:
-                if minutes / 5 == hours:
+                if hours*5+round(minutes/60*5) == minutes and hour_meet == False:
+                    hour_meet = True
                     # print(hours + 12 * i, minutes, end="|")
                     count += 1
                 # print(hours + 12 * i, minutes, end="|")
@@ -26,10 +27,12 @@ def count_meet_hour_and_minute_hands(start_hour, start_minute, end_hour, end_min
         i += 1
     return count
 
+
 start_hour = 0
 start_minute = 5
 end_hour = 23
-end_minute = 55
+end_minute = 59
 
-count_meet1 = count_meet_hour_and_minute_hands(start_hour, start_minute, end_hour, end_minute)
+count_meet1 = count_meet_hour_and_minute_hands(
+    start_hour, start_minute, end_hour, end_minute)
 print(f"\n Часовая и минутная стрелки сходятся раз: {count_meet1}")
